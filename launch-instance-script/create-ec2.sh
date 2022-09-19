4c31b30513b512156312c5fdb44ccfd41ab9513d
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-19 20:52:26 +0530

    updating ./roboshop_ansible

--- launch-instance-script/create-ec2.sh
+++ launch-instance-script/create-ec2.sh
@@ -23,7 +23,7 @@ create_ec2() {
       --iam-instance-profile="${IAM_INSTANCE_PROFILE}" \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}${ENV}/" route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 }
 
