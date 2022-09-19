f46611013ab3c87cf171459fd754ad0d1d1930c4
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-19 20:37:38 +0530

    updating ./roboshop_ansible

--- launch-instance-script/create-ec2.sh
+++ launch-instance-script/create-ec2.sh
@@ -7,12 +7,17 @@ IAM_INSTANCE_PROFILE="Arn=arn:aws:iam::645019601948:instance-profile/role-for-aw
 #############################
 
 
+if [ -z "${1}" ]; then
+  ENV=""
+else
+  ENV="-$1"
+fi
 COMPONENT=all
 create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
       --instance-type t2.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}${ENV}}]" \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
       --iam-instance-profile="${IAM_INSTANCE_PROFILE}" \
