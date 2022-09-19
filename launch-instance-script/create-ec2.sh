84a3e2911b6c4026ddccdf85c525d52537ef3c8d
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-19 11:03:16 +0530

    updating ./roboshop_ansible

--- launch-instance-script/create-ec2.sh
+++ launch-instance-script/create-ec2.sh
@@ -3,7 +3,7 @@
 ##### Change these values ###
 ZONE_ID="Z078807431WCP5BA26ASG"
 SG_NAME="allow-all-to-public"
-IAM_INSTANCE_PROFILE="Arn=arn:aws:iam::645019601948:instance-profile/role-for-Work-station"
+IAM_INSTANCE_PROFILE="Arn=arn:aws:iam::645019601948:instance-profile/role-for-aws-secret-roboshop-COMPONENTS"
 #############################
 
 
